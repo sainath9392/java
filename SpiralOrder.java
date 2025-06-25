fc3d4dde6ebbe299b5af829708168e84de99f78f
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-25 23:32:06 +0530

    Add SpiralOrder.java to implement a method for returning elements of a matrix in spiral order

---
+++ SpiralOrder.java
@@ -0,0 +1,13 @@
+import java.nio.Buffer;
+import java.util.*;
+public class SpiralOrder {
+    public static int [] spiralOfMatrix(int [][] matrix){
+        List<Integer> list = new ArrayList<>();
+        int top = 0,bottom = matrix.length-1;
+        int left = 0,right = matrix[0].length - 1;
+
+        while(top <= bottom && left <= right){
+            for(int top = 0,buttom >)
+        }
+    }
+}
