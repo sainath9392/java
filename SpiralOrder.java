5756f59d2a8ff48b3b556f0a1234d90e2e1cb426
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-26 07:51:50 +0530

    Refactor spiralOfMatrix method to return a List<Integer> and improve readability

--- SpiralOrder.java
+++ SpiralOrder.java
@@ -1,13 +1,49 @@
-import java.nio.Buffer;
 import java.util.*;
 public class SpiralOrder {
-    public static int [] spiralOfMatrix(int [][] matrix){
-        List<Integer> list = new ArrayList<>();
+    public static List<Integer> spiralOfMatrix(int [][] matrix){
+
+        List<Integer> result = new ArrayList<>();
+
         int top = 0,bottom = matrix.length-1;
         int left = 0,right = matrix[0].length - 1;
 
         while(top <= bottom && left <= right){
-            for(int top = 0,buttom >)
+
+            //left to right
+            for(int i = left;i <= right;i++){
+                result.add(matrix[top][i]);
+            }
+            top++;
+
+            //top to bottom
+            for(int i = top;i<=bottom;i++){
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+            //right to left
+            if(top<=bottom){
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            //bottom to top
+           if(left <= right){
+            for(int i = bottom;i>=top;i--){
+                result.add(matrix[i][left]);
+            }
+            left++;
+           }
+
         }
+        return  result;
+    }
+
+    public static void main(String[] args) {
+        int [][] matrix= {{1,2,3},{4,5,6},{7,8,9}};
+        List<Integer> result = spiralOfMatrix(matrix);
+        System.out.println(result);
     }
 }
